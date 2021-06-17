5994bfbfaa4832249681cf62c0296e09f0a40e56
icld <ian@icldesign.com>
2021-06-17 06:47:37 -0700

    add error routes and handling

--- app.js
+++ app.js
@@ -1,41 +1,68 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const { sequelize, Books } = require("./models");
+
+(async () => {
+  await sequelize.sync();
+  try {
+    await sequelize.authenticate();
+    console.log("We have a successful connection");
+  } catch (error) {
+    console.error("Trouble connection to the database: ", error);
+  }
+})();
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+  const err = new Error();
+  err.status = 404;
+  err.message = "Sorry! We couldn't find what you were looking for!";
+  res.status(404).render("page-not-found", { error: err });
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
+});
+
+//my Global Error Handler
+app.use((err, req, res, next) => {
+  if (err.status === 404) {
+    res.status(404).render("page-not-found", { error: err });
+  } else {
+    err.message = err.message || "Something failed!!";
+    err.status = err.status || 500;
+    console.log(err.message);
+    res.status(500).render("error", { error: err });
+  }
+  console.log(err.message);
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const { sequelize, Book } = require("../models");
+
+/* Handler function to wrap each route. */
+function asyncHandler(cb) {
+  return async (req, res, next) => {
+    try {
+      await cb(req, res, next);
+    } catch (error) {
+      // Forward error to the global error handler
+      next(error);
+    }
+  };
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const books = await Book.findAll();
+    // throw Error(500);
+    // const allBooks = books.toJSON()
+    // console.log(books);
+
+    res.render("index", { title: "what you want?" });
+  })
+);
 
 module.exports = router;
